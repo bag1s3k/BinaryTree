9b545ca380d5b8305629058dcad3962bf66efa7a
maty <maty.idk@emai.cz>
2025-05-07 23:29:35 +0200

    add delete in HeapTree

--- BinaryTree/HeapTree.cs
+++ BinaryTree/HeapTree.cs
@@ -1,6 +1,7 @@
 ﻿namespace BinaryTree;
 
 using System.Collections.Generic;
+using System.Net.Mail;
 
 public class HeapTreeMin {
     private List<int> heap;
@@ -21,7 +22,31 @@ public class HeapTreeMin {
             } else 
                 break;
         }
-        
+    }
+
+    public bool Delete(int value) {
+        int i = heap.IndexOf(value);
+        if (i == -1) return false;
+        heap[i] = heap[heap.Count - 1];
+        heap.RemoveAt(heap.Count - 1);
+
+        while (true) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int smallest = i;
+
+            if (left < heap.Count && heap[left] < heap[smallest])
+                smallest = left;
+            if (right < heap.Count && heap[right] < heap[smallest])
+                smallest = right;
+            if (smallest == i)
+                break;
+
+            (heap[i], heap[smallest]) = (heap[smallest], heap[i]);
+            i = smallest;
+        }
+
+        return true;
     }
 
     public override string ToString() {
