778520c2425b1e20383abd2963f9cd1525fc47c6
maty <maty.idk@emai.cz>
2025-05-07 22:19:32 +0200

    add heap

--- BinaryTree/HeapTree.cs
+++ BinaryTree/HeapTree.cs
@@ -1,5 +1,33 @@
 ﻿namespace BinaryTree;
 
-public class HeapTree {
+using System.Collections.Generic;
+
+public class HeapTreeMin {
+    private List<int> heap;
     
+    public HeapTreeMin() => this.heap = new List<int>(); // constructor
+
+    public void Add(int value) {
+        heap.Add(value);
+
+        int currentIndex = heap.Count - 1;
+
+        while (currentIndex > 0) {
+            int parentIndex = (currentIndex - 1) / 2;
+
+            if (heap[parentIndex] > heap[currentIndex]) {
+                (heap[parentIndex], heap[currentIndex]) = (heap[currentIndex], heap[parentIndex]);
+                currentIndex = parentIndex;
+            } else 
+                break;
+        }
+        
+    }
+
+    public override string ToString() {
+        string temp = "";
+        for (int i = 0; i < heap.Count; i++)
+            temp += heap[i] + " ";
+        return temp;
+    }
 }
