330aeacfd866a1590daf4bb15ac96af9c719ff14
maty <maty.idk@emai.cz>
2025-05-07 20:18:54 +0200

    add .Delete()

--- BinaryTree/BaseTree.cs
+++ BinaryTree/BaseTree.cs
@@ -16,4 +16,36 @@ public abstract class TreeBase {
 
         return SearchRecursive(node.Left, value) || SearchRecursive(node.Right, value);
     }
+
+    public void Delete(int value) => Root = DeleteRecursive(Root, value);
+
+    private Node DeleteRecursive(Node node, int value) {
+        if (node == null)
+            return null;
+
+        if (value < node.Value)
+            node.Left = DeleteRecursive(node.Left, value);
+        else if (value > node.Value)
+            node.Right = DeleteRecursive(node.Right, value);
+        else {
+            if (node.Left == null)
+                return node.Right;
+            else if (node.Right == null)
+                return node.Left;
+            
+            Node minNode = FindMin(node.Right);
+            node.Value = minNode.Value;
+            node.Right = DeleteRecursive(node.Right, minNode.Value);
+        }
+
+
+        return node;
+    }
+
+    private Node FindMin(Node node) {
+        while (node.Left != null)
+            node = node.Left;
+
+        return node;
+    }
 }
